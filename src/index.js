11ae3ad9bbac99d0ed705cb32ec8c4f8eea15fc8
Vali Manu <manu_vali85@yahoo.com>
2021-04-10 17:26:42 +0300

    v3.0

--- src/index.js
+++ src/index.js
@@ -2,156 +2,132 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 const Square = (props) => {
-    const [row, setRow] = useState('');
-    const [col, setCol] = useState('');
-    const [bomb, setBomb] = useState('');
-    const lines = rangeArray(0, row);
-    const columns = rangeArray(0, col);
+    return (
+        <button
+            className='square'
+            id={props.id}
+            readOnly
+            onClick={props.onClick}
+            value={props.value}
+            line={props.line}
+            row={props.row}
+        > {props.value}
+        </button>
+    )
+}
+
+const Board = (props) => {
+    const bombsArray = bombsArr(props.line, props.row, props.bomb);
     let bombFree = 0;
-    
-    let board = rangeArray(0, col * row);
-    let copyBom = bomb;
-    for (let i = 0; i < board.length; i++) {
-        if (copyBom) {
-            board[i] = '*';
-            --copyBom;
-        }
-        else {
-            board[i] = 0;
-        }
-    };
-
-    board = shuffleArray(board).slice();
-    let bidimensionalArray = new Array(row && col ? row : 100);
-    for (let i = 0; i < row; i++) {
-        bidimensionalArray[i] = new Array(col && row ? col: 100);
-    }
-
-    if (row && col) {
-        let k = 0;
-        for (let i = 0; i < row; i++) {
-            for (let j = 0; j < col; j++) {
-                bidimensionalArray[i][j] = board[k];
-                ++k;
-            }
-        }
-        for (let i = 0; i < row; i++) {
-            for (let j = 0; j < col; j++) {
-                if (!bidimensionalArray[i][j]) {
-                    let numberOfBombs = 0;
-                    if (i > 0 && j > 0 && bidimensionalArray[i - 1][j - 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (i > 0 && bidimensionalArray[i - 1][j] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (i > 0 && j + 1 < col && bidimensionalArray[i - 1][j + 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (j > 0 && bidimensionalArray[i][j - 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (j + 1 < col && bidimensionalArray[i][j + 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (i + 1 < row && j > 0 && bidimensionalArray[i + 1][j - 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (i + 1 < row && bidimensionalArray[i + 1][j] === "*") {
-                        ++numberOfBombs;
-                    }
-                    if (i + 1 < row && j + 1 < col && bidimensionalArray[i + 1][j + 1] === "*") {
-                        ++numberOfBombs;
-                    }
-                    bidimensionalArray[i][j] = numberOfBombs;
-                }
-
-            }
-        }
-    }
-   
-    function displayBoard(e) {
+    function handleClick(e) {
         e.preventDefault();
-        setRow(document.getElementById("1").value);
-        setCol(document.getElementById("2").value);
-        setBomb(document.getElementById("3").value);
-    }
-
-    function isZero(id) {
-        for (let i = -1; i <= 1; i++) {
-            for (let j = -1; j <= 1; j++) {
-                let newId = "#" + (parseInt(id[1]) + i) + (parseInt(id[2]) + j);
-                if(document.getElementById(newId) && document.getElementById(newId).style.color !== "black") {
-                    document.getElementById(newId).style.color = "black";
-                    ++bombFree;
-                    if(document.getElementById(newId).value === "0") {
-                        isZero(newId);
-                    }
-                }
-            }
-        }
-    }
-
-    function sowValue(e) {
-        e.preventDefault();
-        let id = e.target.id;
         if (e.target.value === "0") {
-            isZero(id);
+            isZero(e.target.getAttribute('line'), e.target.getAttribute('row'));
         }
-        if(e.target.value === "*") {
+        if (e.target.value === "*") {
             alert("Game Over");
         }
-        if(e.target.style.color !== 'black') {
-            ++bombFree;
+        if (e.target.style.color !== 'black') {
+            ++bombFree
         }
         e.target.style.color = 'black';
-        if(bombFree === col * row - bomb) {
+        if (bombFree === props.line * props.row - props.bomb) {
             alert("Winner!")
         }
     }
-    
+    function isZero(line, row) {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                let newLine = parseInt(line) + i;
+                let newRow = parseInt(row) + j;
+                let id = newLine + "#" + newRow;
+                if (document.getElementById(id) && document.getElementById(id).style.color !== "black") {
+                    document.getElementById(id).style.color = "black";
+                    ++bombFree;
+                    if (document.getElementById(id).value === "0") {
+                        isZero(newLine, newRow, bombFree);
+                    }
+                }
+            }
+        }
+    }
     return (
-        <div>
-            <form onSubmit={displayBoard}>
-                <input type="number" placeholder="row nr." id="1" />
-                <input type="number" placeholder="col nr." id="2" />
-                <input type="number" placeholder="bomb nr." id="3" />
+        range(props.line).map(i => {
+            return (
+                <div key={i} className="board-row">
+                    {
+                        range(props.row).map(j => <Square key={j} value={bombsArray[i][j]} id={i + "#" + j} onClick={handleClick} line={i} row={j} />)
+                    }
+                </div>
+            )
+        })
+    )
+}
+
+const InputForm = (props) => {
+    const [lines, setLine] = useState();
+    const [rows, setRow] = useState();
+    const [bombs, setBombs] = useState();
+    function handleSubmit(e) {
+        e.preventDefault();
+        setLine(parseInt(document.getElementById('#1').value));
+        setRow(parseInt(document.getElementById('#2').value));
+        setBombs(parseInt(document.getElementById('#3').value));
+
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="number" placeholder="Height" value={props.lines} id='#1' />
+                <input type="number" placeholder="Width" value={props.rows} id='#2' />
+                <input type="number" placeholder="Mines" value={props.bombs} id="#3" />
                 <button>Play</button>
                 <button onClick={() => window.location.reload()}>Reset Game</button>
             </form>
-            <div>
-                {
-                    lines.map(line => {
-                        return <div key={line} className="board-row">
-                            {
-                                columns.map(col => {
-                                    return <input key={col} className='square' id={"#" + line + col} onClick={sowValue} value={bidimensionalArray[line][col]} readOnly />
-                                })
-                            }
-                        </div>
-                    })
+            <Board line={lines} row={rows} bomb={bombs} />
+        </>
+    );
+}
 
-                }
-            </div>
+const App = (props) => {
+    return (
+        <div>
+            <h3>Minesweeper Game</h3>
+            <InputForm />
         </div>
     )
 }
 
 ReactDOM.render(
-    <div>
-        <div>Minesweeper Game</div>
-        <Square />
-    </div>,
+    <App />,
     document.getElementById('root'),
 );
 
-function rangeArray(start, end) {
-    const line = [];
-    for (let i = start; i < end; i++) {
-        line[i] = i;
+// ***UTILS***
+function range(max) {
+    const arr = [];
+    for (let i = 0; i < max; i++) {
+        arr[i] = i;
+    };
+    return arr;
+};
+
+function bombsArr(lines, rows, bombs) {
+    let arr = range(lines * rows);
+    for (let i = 0; i < arr.length; i++) {
+        if (bombs) {
+            arr[i] = '*';
+            --bombs;
+        } else {
+            arr[i] = 0;
+        }
     }
-    return line;
+    arr = shuffleArray(arr).slice();
+    arr = gameBoard(lines, rows, arr).slice();
+    countBombs(lines, rows, arr);
+    return arr;
 }
 
 function shuffleArray(array) {
@@ -164,3 +140,52 @@ function shuffleArray(array) {
     }
     return array;
 }
+
+function gameBoard(lines, rows, bombsArr) {
+    let boardGame = [[]];
+    let k = 0;
+    for (let i = 0; i < lines; i++) {
+        boardGame[i] = range(rows);
+        for (let j = 0; j < rows; j++) {
+            boardGame[i][j] = bombsArr[k];
+            ++k;
+        }
+    }
+    return boardGame;
+}
+
+function countBombs(lines, rows, arr) {
+    for (let i = 0; i < lines; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (!arr[i][j]) {
+                let numberOfBombs = 0;
+                if (i > 0 && j > 0 && arr[i - 1][j - 1] === "*") {
+                    ++numberOfBombs;
+                }
+                if (i > 0 && arr[i - 1][j] === "*") {
+                    ++numberOfBombs;
+                }
+                if (i > 0 && j + 1 < rows && arr[i - 1][j + 1] === "*") {
+                    ++numberOfBombs;
+                }
+                if (j > 0 && arr[i][j - 1] === "*") {
+                    ++numberOfBombs;
+                }
+                if (j + 1 < rows && arr[i][j + 1] === "*") {
+                    ++numberOfBombs;
+                }
+                if (i + 1 < lines && j > 0 && arr[i + 1][j - 1] === "*") {
+                    ++numberOfBombs;
+                }
+                if (i + 1 < lines && arr[i + 1][j] === "*") {
+                    ++numberOfBombs;
+                }
+                if (i + 1 < lines && j + 1 < rows && arr[i + 1][j + 1] === "*") {
+                    ++numberOfBombs;
+                }
+                arr[i][j] = numberOfBombs;
+            }
+        }
+    }
+}
+
